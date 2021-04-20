425423f8120bdd9223459063559cacbe0a95dd1c
Xie <jxie@dxc.com>
2021-04-20 15:18:46 +0800

    feat: hang

---
+++ src/pages/calculator/bazi/index.config.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: qiuz
+ * @Github: <https://github.com/qiuziz>
+ * @Date:  2020-12-09 23:38:37
+ * @Last Modified by: qiuz
+ */
+
+export default {
+  navigationBarTitleText: "白字排盘",
+  navigationBarTextStyle: "black",
+  navigationBarBackgroundColor: "#fff"
+};
